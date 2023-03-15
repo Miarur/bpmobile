59c83f276c4a60ed75fcb2d8489a3fedda460544
Andrey Smolko <andrey.smolko.dev@gmail.com>
2023-03-15 13:47:45 +0300

    feat: add basic files

---
+++ src/Header/index.js
@@ -0,0 +1,6 @@
+import htmlToElement from '../utils/htmlToElement';
+import Header from './header.html';
+import './header.css';
+
+const header = htmlToElement(Header);
+export default header;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ]
+  },
+  devServer: {
+    compress: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: './src/index.html',
+        filename: './index.html',
+      })
+  ],
+  optimization: {
+    minimize: true,
+  }
+}
